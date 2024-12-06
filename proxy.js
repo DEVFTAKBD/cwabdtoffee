3e7943ca1f6433a96ebd5cfd0a5dce1c4518ff19
Farhan Tanvir Arnob Khan <132578643+DEVFTAKBD@users.noreply.github.com>
2024-12-06 11:49:41 +0600

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,24 +1,15 @@
-const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const app = express();
-const PORT = 3000;
-
-// Proxy middleware to add the Referer header
-app.use(
-  '/proxy',
-  createProxyMiddleware({
-    target: 'https://bdixtv24.site',
+module.exports = (req, res) => {
+  const proxy = createProxyMiddleware({
+    target: 'https://bdixtv24.site', // The actual target
     changeOrigin: true,
-    pathRewrite: {
-      '^/proxy': '', // Remove '/proxy' from the request URL
-    },
+    pathRewrite: { '^/api/proxy': '' }, // Rewrite API route
     onProxyReq: (proxyReq) => {
       proxyReq.setHeader('Referer', 'https://bdixtv24.site/');
     },
-  })
-);
+  });
 
-app.listen(PORT, () => {
-  console.log(`Proxy server is running on http://localhost:${PORT}`);
-});
+  return proxy(req, res); // Pass request to proxy
+};
+
