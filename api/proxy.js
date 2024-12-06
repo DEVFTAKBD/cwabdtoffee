0fd717d0faee1161a699df6333fda09ab172a4b7
Farhan Tanvir Arnob Khan <132578643+DEVFTAKBD@users.noreply.github.com>
2024-12-06 11:50:21 +0600

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = (req, res) => {
+  const proxy = createProxyMiddleware({
+    target: 'https://bdixtv24.site', // The actual target
+    changeOrigin: true,
+    pathRewrite: { '^/api/proxy': '' }, // Rewrite API route
+    onProxyReq: (proxyReq) => {
+      proxyReq.setHeader('Referer', 'https://bdixtv24.site/');
+    },
+  });
+
+  return proxy(req, res); // Pass request to proxy
+};
